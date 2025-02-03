bdf075a47219d3bb7c8bd65cc55d9f6db9949782
mkaseeram <mkaseeram@gmail.com>
2025-02-03 08:27:28 +0530

    Updated App.js and resolved image loading issue

--- src/App.js
+++ src/App.js
@@ -65,14 +65,13 @@ function App() {
       </div>
       <div className="main-content">
         <div className="image-container">
-          {!sidebarOpen && <button className="toggle-btn btn" onClick={() => setSidebarOpen(!sidebarOpen)}>
+          {!sidebarOpen && <button className="toggle-btn" onClick={() => setSidebarOpen(!sidebarOpen)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
             </svg>
           </button>}
-            <img src="/Maitreya.png" alt="Statue" className="statue-image" />
+          <img src={`${process.env.PUBLIC_URL}/statue.png`} alt='statue-image' className='statue-image'/>
         </div>
-        
       </div>
     </div>
   );
